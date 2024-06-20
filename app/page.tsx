c7c073f026214cee6ee516b0dce5e19df04f6d0c
Srivishal Sivasubramanian <srivishal.sivasubramanian@gmail.com>
2024-06-20 16:00:30 +0530

    task: hero background

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,15 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-200 px-5 sm:px-10">
+      <div className="w-full max-w-7xl">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
-        <RecentProjects />
-        <Clients />
-        <Experience />
-        <Approach />
+        {/* <Grid /> */}
+        {/* <RecentProjects /> */}
+        {/* <Clients /> */}
+        {/* <Experience /> */}
+        {/* <Approach /> */}
         <Footer />
       </div>
     </main>
